eacfe4e3e9b90cf0cb95749582b900b875ba7bcf
Travis Tidwell <travist@alethia-inc.com>
2011-10-19 08:58:47 -0500

    Adding more unit tests

--- js/test/allplayers.event.test.js
+++ js/test/allplayers.event.test.js
@@ -1,5 +1,48 @@
 (function($) {
   $(function() {
 
+    allplayers.test(function() {
+
+      // Create a new api object.
+      var api = new allplayers.api({
+        api_path: 'https://www.pdup.allplayers.com/api/v1/rest'
+      });
+
+      // Test api.getGroupEvents
+      asyncTest("api.getGroupEvents()", 2, function() {
+        api.searchGroups({search: 'Spring Soccer 2011'}, function(groups) {
+          api.getGroupEvents(groups[0].uuid, {
+            start: '2011-10-1',
+            end: '2011-10-30'
+          }, function(events) {
+
+            this.start();
+
+            // Iterate through each event and verify that it is in the right
+            // timeframe.
+            var passed = true, uuids = true;
+            var i = events.length, start = null, end = null;
+            while (i--) {
+              start = new Date(events[i].start).getMonth() + 1;
+              end = new Date(events[i].end).getMonth() + 1;
+              if (start != 10 && end != 10) {
+                passed = false;
+              }
+              if (!events[i].uuid) {
+                uuids = false;
+              }
+            }
+
+            ok(passed, "Events in correct timeframe");
+            ok(uuids, "All events have UUIDS");
+
+            allplayers.testDone();
+          });
+
+        });
+      });
+
+    });
+
   });
 }(jQuery));
